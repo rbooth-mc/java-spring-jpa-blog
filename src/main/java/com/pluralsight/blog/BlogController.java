20e60fabc1b51b12438d6f54a1c24d81e800f146
Randall Booth <rbooth@mastercontrol.com>
2021-07-14 09:31:36 -0600

    Module 3 - Display all categories and individual categories

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -4,6 +4,7 @@ import com.pluralsight.blog.data.CategoryRepository;
 import com.pluralsight.blog.data.PostRepository;
 import com.pluralsight.blog.model.Category;
 import com.pluralsight.blog.model.Post;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,15 +17,19 @@ import java.util.Optional;
 public class BlogController {
 
     private PostRepository postRepository;
+    private CategoryRepository categoryRepository;
 
-    public BlogController(PostRepository postRepository) {
+    public BlogController(PostRepository postRepository, CategoryRepository categoryRepository) {
         this.postRepository = postRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap) {
         List<Post> posts = postRepository.findAll();
         modelMap.put("posts", posts);
+        List<Category> categories = categoryRepository.findAll();
+        modelMap.put("categories", categories);
         return "home";
     }
 
@@ -33,5 +38,16 @@ public class BlogController {
         Post post = postRepository.findById(id).orElse(null);
         modelMap.put("post", post);
         return "post-details";
+    }
+
+    @RequestMapping("/category/{id}")
+    public String categoryList(@PathVariable Long id, ModelMap modelMap) {
+        Category category = categoryRepository.findById(id).orElse(null);
+        modelMap.put("category", category);
+        List<Post> posts = postRepository.findByCategory(category);
+        modelMap.put("posts", posts);
+        List<Category> categories = categoryRepository.findAll();
+        modelMap.put("categories", categories);
+        return "category-list";
     }
 }
